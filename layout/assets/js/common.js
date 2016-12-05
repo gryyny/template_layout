fc372bb5513858602d244c1226775b7a151302c7
Andryi Grinchuck <gryyny@gmail.com>
2016-12-05 19:05:35 +0200

    init commit

---
+++ layout/assets/js/common.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+    // Elements to inject
+    var mySVGsToInject = document.querySelectorAll('img.inject-me');
+
+    // Do the injection
+    SVGInjector(mySVGsToInject);
+
+    // popups init
+    $('.popup-wr').each(function () {
+        var self = $(this);
+        self.on('click','.popup-overlay, .close', function (e) {
+            e.preventDefault();
+            closePopup(self.attr('id'));
+        });
+    });
+
+    $('[data-popup]').each(function () {
+        $(this).on('click', function (e) {
+            e.preventDefault();
+            showPopup($(this).attr('data-popup'));
+        });
+    });
+});
+
+function showPopup(id) {
+    if (id.substring(0, 1) != '#') {
+        id = '#' + id;
+    }
+    $(id).addClass('show effect');
+}
+
+function closePopup(id) {
+    if (id.substring(0, 1) != '#') {
+        id = '#' + id;
+    }
+    $(id).removeClass('effect');
+    setTimeout(function () {
+        $(id).removeClass('show');
+    },500);
+}
---
+++ layout/gulpfile.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const gulp = require('gulp');
+const browserSync = require('browser-sync');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const gutil = require('gulp-util');
+
+var reload = browserSync.reload;
+
+var src = {
+    scss: 'assets/sass/**/*.scss',
+    css: 'assets/css',
+
+    html: ['*.html', 'Views/**/*.cshtml'],
+    js: 'assets/js/**/*.js'
+};
+
+gulp.task('serve', ['sass'], function () {
+
+    browserSync.init({
+        server: "./",
+        port: 3033
+    });
+
+    // browserSync({
+    //     startPath: "/",
+    //     proxy: "localhost:17725"
+    // });
+
+    gulp.watch(src.scss, ['sass']);
+    gulp.watch(src.html).on('change', reload);
+    gulp.watch(src.js).on('change', reload);
+});
+
+gulp.task('sassWatch', ['sass'], function() {
+    gulp.watch(src.scss, ['sass']);
+});
+
+// Compile sass into CSS
+gulp.task('sass', function () {
+    var s = sass.sync({
+        outputStyle: 'compact'
+    });
+    s.on('error',function(e){
+        gutil.log(e);
+        s.end();
+    });
+    return gulp.src(src.scss)
+        .pipe(s)
+        .pipe(autoprefixer({
+            browsers: ['last 50 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest(src.css))
+        .pipe(browserSync.stream());
+});
+gulp.task('default', ['serve']);
